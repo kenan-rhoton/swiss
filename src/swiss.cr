9ada15ac8d8fb755cd58881c641642601c6d4b1b
Kenan Rhoton <krhoton@Kenan-Rhoton.local>
2018-04-27 09:15:22 +0200

    2 more rounds

--- src/swiss.cr
+++ src/swiss.cr
@@ -14,13 +14,13 @@ module Swiss
         "Cristian",
         "Axel",
         "Eric",
-        "Gerard"
+        "Gerard P"
     ]
     t = Tournament::Swiss.new(players)
     # Round 1
     t.add_results([
         {"Hugo" => 1.0, "Pol" => 0.0},
-        {"Gerard" => 1.0, "Eric" => 0.0},
+        {"Gerard P" => 1.0, "Eric" => 0.0},
         {"Ana" => 0.5, "Gabriela" => 0.5},
         {"Raúl" => 1.0, "Axel" => 0.0},
         {"Cristian" => 0.5, "Marc" => 0.5},
@@ -29,30 +29,50 @@ module Swiss
     # Round 2
     t.add_results([
         {"Hugo" => 0.5, "Raúl" => 0.5},
-        {"Jordana" => 0.5, "Gerard" => 0.5},
+        {"Jordana" => 0.5, "Gerard P" => 0.5},
         {"Marc" => 0.5, "Gabriela" => 0.5},
         {"Ana" => 0.5, "Cristian" => 0.5},
         {"Pol" => 0.5, "Antonio" => 0.5},
         {"Axel" => 0.5, "Eric" => 0.5},
     ])
 
-    # Round 2
+    # Round 3
     t.add_results([
         {"Hugo" => 0.5, "Jordana" => 0.5},
-        {"Raúl" => 0.5, "Gerard" => 0.5},
+        {"Raúl" => 0.5, "Gerard P" => 0.5},
         {"Pol" => 0.5, "Axel" => 0.5},
         {"Ana" => 1.0, "Antonio" => 0.0},
         {"Cristian" => 0.5, "Gabriela" => 0.0},
         {"Marc" => 0.0, "Eric" => 0.0},
     ])
 
-    puts "\n\nStandings:"
+    # Round 4
+    t.add_results([
+        {"Hugo" => 0.5, "Raúl" => 0.5},
+        {"Cristian" => 0.5, "Gerard P" => 0.5},
+        {"Pol" => 1.0, "Marc" => 0.0},
+        {"Gabriela" => 0.5, "Axel" => 0.5},
+        {"Antonio" => 0.0, "Eric" => 1.0},
+        {"Ana" => 0.0, "Jordana" => 0.0},
+    ])
+
+    # Round 5
+    t.add_results([
+        {"Gabriela" => 0.5, "Eric" => 0.5},
+        {"Antonio" => 0.5, "Marc" => 0.5},
+        {"Gerard P" => 0.5, "Hugo" => 0.5},
+        {"Cristian" => 1.0, "Axel" => 0.0},
+        {"Gerard C" => 0.5, "Raúl" => 0.5},
+        {"Ana" => 0.0, "Jordana" => 0.0},
+    ])
+
+    puts "Puntuaciones:"
     t.standings.each do |standing|
         puts "#{standing[1]} - #{standing[0]}"
     end
 
     pairings = t.generate_pairings
-    puts "\n\nPairings:"
+    puts "\nEmparejamientos:"
     pairings.each do |pair|
         puts "#{pair[:white]} vs #{pair[:black]}"
     end
